3fdba4cae879386c38d4c28d673498b0e4d6ef7c
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-09-29 09:57:53 +0530

    Time: 203 ms (13.54%), Space: 37.4 MB (11.15%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,15 +1,32 @@
+#define pp pair<int,int>
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>>p; // condition(|a-x|) , number itself is stored
+        // using max heap
+        // priority_queue<pair<int,int>>p; // condition(|a-x|) , number itself is stored
+        // for(int i=0;i<arr.size();i++)
+        // {
+        //     p.push({abs(x-arr[i]), arr[i]});
+        //     if(p.size()>k)
+        //         p.pop();
+        // }
+        // vector<int>ans;
+        // while(p.size()>0)
+        // {
+        //     ans.push_back(p.top().second);
+        //     p.pop();
+        // }
+        // sort(ans.begin(),ans.end());
+        
+        // using min heap
+        priority_queue<pp,vector<pp>, greater<pp>>p; // condition(|a-x|) , number itself is stored
+        
         for(int i=0;i<arr.size();i++)
         {
             p.push({abs(x-arr[i]), arr[i]});
-            if(p.size()>k)
-                p.pop();
         }
         vector<int>ans;
-        while(p.size()>0)
+        while(p.size()>(arr.size()-k))
         {
             ans.push_back(p.top().second);
             p.pop();
