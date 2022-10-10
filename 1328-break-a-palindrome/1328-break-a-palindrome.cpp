d52ef5ce1c3c7cff781424a9a1b5a17622d11759
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-10 15:03:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    bool isPalin(string s)
+    {
+        int st=0, e=s.size()-1;
+        while(st<=e)
+        {
+            if(s[st]!=s[e]) return 0;
+            st++;
+            e--;
+        }
+        return 1;
+    }
+    
+    string breakPalindrome(string p) {
+        if(p.size() == 1) return "";
+        string ans="";
+        int ct=0;
+        int i;
+        for(i=0;i<p.size();i++)
+        {
+          if(p[i]!='a' && !isPalin(p.substr(0,i)+'a'+p.substr(i+1,p.size())))
+          {
+              ct++;
+              break;
+          }
+        }
+        if(ct==1) p[i] = 'a';
+        else
+        {
+            for(char c='a';c<='z';c++)
+            {
+                if(c!=p[p.size()-1])
+                {
+                    p[p.size()-1] = c;
+                    break;
+                }
+            }
+        }
+        return p;
+    }
+};
