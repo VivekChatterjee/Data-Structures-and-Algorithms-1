ac1ab3a4f8f3f8e90a66edc73f0dd32b0881af11
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-13 18:32:50 +0530

    Time: 9 ms (26.24%), Space: 7.7 MB (55.75%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,57 @@
+// #define vc vector
+// class Solution {
+// public:
+//     vc<vc<int>>ans;
+//     void solve(vc<int>&nums, int freq[] , vc<int>v)
+//     {
+//         if(v.size() == nums.size())
+//         {
+//             ans.push_back(v);
+//             return;
+//         }
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(!freq[i])
+//             {
+//                 freq[i]=1;
+//                 v.push_back(nums[i]);
+//                 solve(nums,freq,v);
+//                 v.pop_back();
+//                 freq[i]=0;
+//             }
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vc<int>v;
+//         int freq[6]={0};
+//         solve(nums,freq,v);
+//         return ans;
+//     }
+// };
+
+
+// 2nd approach
 #define vc vector
 class Solution {
 public:
     vc<vc<int>>ans;
-    void solve(vc<int>&nums, int freq[] , vc<int>v)
+    void solve(vc<int>&nums, int ind)
     {
-        if(v.size() == nums.size())
+        if(ind == nums.size())
         {
-            ans.push_back(v);
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(!freq[i])
-            {
-                freq[i]=1;
-                v.push_back(nums[i]);
-                solve(nums,freq,v);
-                v.pop_back();
-                freq[i]=0;
-            }
+            swap(nums[i], nums[ind]);
+            solve(nums,ind+1);
+            swap(nums[i], nums[ind]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vc<int>v;
-        int freq[6]={0};
-        solve(nums,freq,v);
+        solve(nums,0);
         return ans;
     }
 };
