f29774ec5cf664c299a22883e7574d17c5babe47
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-13 14:18:19 +0530

    Time: 111 ms (21.40%), Space: 51.4 MB (17.26%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,43 @@
+#define vc vector
+class Solution {
+public:
+    vc<vc<int>>ans;
+    
+    void solve(vc<int>& c, int t, vc<int>v, int i)
+    {
+        
+        if(t == 0)
+        {
+            ans.push_back(v);            
+            return;
+        }
+        
+        if(i>=c.size())
+        {
+            return;
+        }
+        
+        
+        if(c[i] <= t)
+        {
+            // picked, staying on current index
+            v.push_back(c[i]);
+            solve(c,t-c[i] ,v,i);
+            
+            // not picked, moving to next index
+            v.pop_back();
+            solve(c,t,v,i+1);
+        }
+        // current element not picked
+        else
+        solve(c,t,v,i+1);
+        
+    }
+        
+    
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+        vc<int>v;
+        solve(c,t,v,0);
+        return ans;
+    }
+};
