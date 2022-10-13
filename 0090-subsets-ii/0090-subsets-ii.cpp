fe49d8295c0fd20394d4d6be8046b02b260542be
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-13 17:22:42 +0530

    Time: 4 ms (69.37%), Space: 8.1 MB (39.88%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+#define vc vector
+class Solution {
+public:
+    vc<vc<int>>ans;
+    void solve(vc<int>&nums,int i, vc<int>v)
+    {
+        ans.push_back(v);
+        for(int j=i;j<nums.size();j++)
+        {
+            if(j!=i && nums[j]==nums[j-1]) continue;
+            // take current
+            v.push_back(nums[j]);
+            solve(nums,j+1,v);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vc<int>v;
+        solve(nums, 0, v);
+        return ans;
+    }
+};
