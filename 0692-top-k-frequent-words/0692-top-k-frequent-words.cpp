437c952a86e171cd2ccb6e471274bede2855b611
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-19 19:24:43 +0530

    Time: 34 ms (21.17%), Space: 12.8 MB (18.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -3,21 +3,16 @@ public:
     bool static compare(pair<int,string> a, pair<int,string> b)
     {
         if(a.first == b.first)
-        {
             return a.second < b.second;
-        }
         return a.first > b.first;
     }
-    
     vector<string> topKFrequent(vector<string>& words, int k) {
         map<string,int>m;
         for(auto i:words)
             m[i]++;
         vector<pair<int,string>>v;
         for(auto it:m)
-        {
             v.push_back({it.second, it.first});
-        }
         sort(v.begin(),v.end(),compare);
         vector<string>ans;
         for(int i=0;i<k;i++)
