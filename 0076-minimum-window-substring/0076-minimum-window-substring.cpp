40be897c9c138a5c32e455e84ab615b5ab6cfa3b
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-22 13:33:07 +0530

    Time: 40 ms (47.96%), Space: 16.5 MB (15.22%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -8,7 +8,7 @@ public:
         int i=0,j=0;
         int ans=INT_MAX;
         string res="", cur="";
-        while(i<=j && j<s.size())
+        while(j<s.size())
         {
             if(m.count(s[j]))
             {
@@ -16,12 +16,13 @@ public:
                 if(m[s[j]] == 0)
                     count--;
             }
-    
             while(count == 0 && i<=j)
             {
                 if(j-i+1 < ans)
+                {
                     res = s.substr(i, j-i+1);
-                ans= min(ans, j-i+1);
+                    ans= min(ans, j-i+1);
+                }
                 if(m.count(s[i]))
                 {
                     m[s[i]]++;
