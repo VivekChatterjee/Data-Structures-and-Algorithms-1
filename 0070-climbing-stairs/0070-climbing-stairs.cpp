be360ee962e0fec2c9eda722d747cbcfa51f2401
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-26 20:17:04 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=2;
+        // SC = O(n)
+        // vector<int> dp(n+1,-1);
+        // dp[0]=1;
+        // dp[1]=2;
+        // for(int i=2;i<n;i++)
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // return dp[n-1];
+        
+        // SC = O(1)
+        int prev2=1;
+        int prev=2;
+        if(n==1) return 1;
         for(int i=2;i<n;i++)
-            dp[i]=dp[i-1]+dp[i-2];
-        return dp[n-1];
+        {
+            int cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
     }
 };
