ac8a7de1dd7160130b8a723568dfa2f6914d04b4
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-26 17:55:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (31.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int climb(int n, vector<int>&dp)
     {
-        if(n<3) return dp[n-1];
+        if(n<3) return n;
         if(dp[n] != -1) return dp[n-1];
         return dp[n-1]=climb(n-1,dp)+climb(n-2,dp);
     }
