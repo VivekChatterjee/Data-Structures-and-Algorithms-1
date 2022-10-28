10980561d8a2b90baa36d59ec168fffc94c51f3b
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-10-29 02:10:42 +0530

    Time: 7 ms (87.99%), Space: 9.1 MB (31.53%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,23 @@
 // tabulation - striver method
-class Solution {
-public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int m=triangle.size();
-        vector<vector<int>>dp(m,vector<int>(m,-1));
-        for(int j=0;j<m;j++)
-        {
-            dp[m-1][j]=triangle[m-1][j];
-        }
-        for(int i=m-2;i>=0;i--)
-        {
-            for(int j=0;j<triangle[i].size();j++)
-            {
-                dp[i][j]=triangle[i][j]+min(dp[i+1][j], dp[i+1][j+1]);
-            }
-        }
-        return dp[0][0];
-    }
-};
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int m=triangle.size();
+//         vector<vector<int>>dp(m,vector<int>(m,-1));
+//         for(int j=0;j<m;j++)
+//         {
+//             dp[m-1][j]=triangle[m-1][j];
+//         }
+//         for(int i=m-2;i>=0;i--)
+//         {
+//             for(int j=0;j<triangle[i].size();j++)
+//             {
+//                 dp[i][j]=triangle[i][j]+min(dp[i+1][j], dp[i+1][j+1]);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
 
 
 // tabulation - my method
@@ -51,26 +51,24 @@ public:
 
 
 // memoization
-// class Solution {
-// public:
-//     int path(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp)
-//     {
-//         int m=triangle.size();
-//         int n=triangle[i].size();
-//         if(j>=n) return 1e9;
-//         if(i == m-1) return triangle[i][j];
-//         if(dp[i][j] != -1) return dp[i][j];
-//         int down=0, downRight=0;
-//         // down
-//         down+=triangle[i][j]+path(i+1,j,triangle,dp);
-//         // down and right
-//         downRight+=triangle[i][j]+path(i+1,j+1,triangle,dp);
-//         return dp[i][j]=min(down,downRight);
-//     }
-//     int minimumTotal(vector<vector<int>>& triangle) {
-//         int m=triangle.size();
-//         int n=triangle[m-1].size();
-//         vector<vector<int>>dp(m,vector<int>(n,-1));
-//         return path(0,0, triangle, dp);
-//     }
-// };
+class Solution {
+public:
+    int path(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp)
+    {
+        int m=triangle.size();
+        int n=triangle[i].size();
+        if(i == m-1) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int down=0, downRight=0;
+        // down
+        down+=triangle[i][j]+path(i+1,j,triangle,dp);
+        // down and right
+        downRight+=triangle[i][j]+path(i+1,j+1,triangle,dp);
+        return dp[i][j]=min(down,downRight);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m=triangle.size();
+        vector<vector<int>>dp(m,vector<int>(m,-1));
+        return path(0,0, triangle, dp);
+    }
+};
