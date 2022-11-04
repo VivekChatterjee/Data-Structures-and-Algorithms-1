038aee524abdc43f71ffca058600ab7ee21d9cb7
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-04 10:09:42 +0530

    Time: 6 ms (26.88%), Space: 8.3 MB (70.56%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,18 +4,20 @@ public:
         int start = 0, mid = 0, end = nums.size()-1;
         while(mid <= end)
         {
-            if(nums[mid] == 0)
-            {
-                swap(nums[start], nums[mid]);
-                start++;
-                mid++;
-            }
-            else if(nums[mid] == 1)
-                mid++;
-            else
+            if(nums[mid] == 2)
             {
                 swap(nums[mid], nums[end]);
                 end--;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[start]);
+                start++;
+                mid++;
             }
         }
     }
