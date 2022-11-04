016675f5e36ab6e6519d8ec633b98a462e3f4f62
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-04 09:45:14 +0530

    Time: 30 ms (53.68%), Space: 13.3 MB (55.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,26 +4,24 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int r=matrix.size();
         int c=matrix[0].size();
-        int colDummy=0;
-        for(int i=0;i<r;i++)
+        vector<int> rowZeroes(r,-1), colZeroes(c,-1);
+        for(int i=0; i<r; i++)
         {
-            if(matrix[i][0]==0) colDummy=1;
-            for(int j=1;j<c;j++)
+            for(int j=0; j<c; j++)
             {
-                if(matrix[i][j]==0)
+                if(matrix[i][j] == 0)
                 {
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    rowZeroes[i]=0;
+                    colZeroes[j]=0;
                 }
             }
         }
-        for(int i=r-1;i>=0;i--)
+        for(int i=0;i<r;i++)
         {
-            for(int j=c-1;j>0;j--)
+            for(int j=0;j<c;j++)
             {
-                if(matrix[0][j]==0 || matrix[i][0]==0) matrix[i][j]=0;
+                if(rowZeroes[i]==0 || colZeroes[j]==0) matrix[i][j]=0;
             }
-            if(colDummy) matrix[i][0]=0;
         }
     }
 };
