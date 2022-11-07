d8daad7b0f3fe7720fb6a8fad2a334abe706f18d
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-08 01:26:38 +0530

    Time: 44 ms (54.20%), Space: 43.7 MB (22.90%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int find(int i, int sum, vector<int>&nums, int target, vector<vector<int>>&dp)
     {
-        if(i == nums.size())
+        if(i == nums.size()-1)
         {
-            if(sum == target) return 1;
+            if(sum + nums[i] == target && sum - nums[i] == target) return 2;
+            else if(sum + nums[i] == target || sum - nums[i] == target) return 1;
             else return 0;
         }
         if(dp[i][sum+1000] != -1) return dp[i][sum+1000];
@@ -19,6 +20,7 @@ public:
         int n=nums.size();
         vector<vector<int>>dp(21, vector<int>(2001, -1));
         return find(0,0,nums,target,dp);
+        // tabulation
     }
 };
 
