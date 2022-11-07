ab8ed90b0d0f103649529dd4f5fe9e8fe1c14108
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-08 00:00:53 +0530

    Time: 1952 ms (12.97%), Space: 54.4 MB (19.90%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    int ans=0;
-    void find(int i, int sum, vector<int>&nums, int target)
+    int find(int i, int sum, vector<int>&nums, int target, vector<vector<int>>&dp)
     {
-        if(i==nums.size())
+        if(i == nums.size())
         {
-            if(sum == target) ans++;
-            return;
+            if(sum == target) return 1;
+            else return 0;
         }
+        
+        if(sum<0)
+        {
+            if(dp[i][sum+2001] != -1) return dp[i][sum+2001];
+        }
+        else
+        {
+            if(dp[i][sum] != -1) return dp[i][sum];
+        }
+        
+        int neg = 0, pos = 0;
+        
         // take -ve
-        find(i+1,sum-nums[i],nums,target);
+        neg = find(i+1,sum-nums[i],nums,target,dp);
+        
         // take +ve
-        find(i+1,sum+nums[i],nums,target);
+        pos = find(i+1,sum+nums[i],nums,target,dp);
+        if(sum < 0) return dp[i][sum+1001] = neg+pos;
+        return dp[i][sum] = neg + pos;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        find(0,0,nums,target);
-        return ans;
+        int n=nums.size();
+        vector<vector<int>>dp(23, vector<int>(3003, -1));
+        return find(0,0,nums,target,dp);
     }
 };
+
+// 20 * -1000 to 20 * 1000
