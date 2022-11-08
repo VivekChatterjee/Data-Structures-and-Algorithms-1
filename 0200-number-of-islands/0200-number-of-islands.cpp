1b40d36badb4ed76bd9d41fbf75bfc864c3cb8d3
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-08 14:18:26 +0530

    Time: 41 ms (89.34%), Space: 13.4 MB (34.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -4,23 +4,12 @@ public:
     {
         int m=grid.size();
         int n=grid[0].size();
+        if(i<0 || j<0 || i>=m || j>=n || vis[i][j] || grid[i][j]=='0') return;
         vis[i][j]=1;
-        if(i+1 < m)
-        {
-            if(!vis[i+1][j] && grid[i+1][j]=='1') dfs(grid, vis, i+1, j);
-        }
-        if(j+1 < n)
-        {
-            if(!vis[i][j+1] && grid[i][j+1]=='1') dfs(grid, vis, i, j+1);
-        }
-        if(i-1>=0)
-        {
-            if(!vis[i-1][j] && grid[i-1][j]=='1') dfs(grid, vis, i-1, j);
-        }
-        if(j-1>=0)
-        {
-            if(!vis[i][j-1] && grid[i][j-1]=='1') dfs(grid, vis, i, j-1);
-        }
+        dfs(grid, vis, i+1, j);
+        dfs(grid, vis, i, j+1);
+        dfs(grid, vis, i-1, j);
+        dfs(grid, vis, i, j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
@@ -39,9 +28,5 @@ public:
             }
         }
         return ans;
-        
-        // 1 1 1
-        // 0 1 0
-        // 1 1 1
     }
 };
