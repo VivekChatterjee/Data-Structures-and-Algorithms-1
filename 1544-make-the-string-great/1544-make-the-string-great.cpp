b6033ed68266156c11270551e9282e11e517e47a
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-08 13:50:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -2,31 +2,53 @@ class Solution {
 public:
     // a to z = 97 to  122
     // A to Z = 65 to 90
+    // string makeGood(string s) {
+    //     int n=s.size();
+    //     for(int i=0; i<n-1; i++)
+    //     {
+    //         if(s[i]>=97)
+    //         {
+    //             if(s[i]-'a' == s[i+1]-'A')
+    //             {
+    //                 s.erase(s.begin()+i);
+    //                 s.erase(s.begin()+i);
+    //                 if(i!=0) i-=2;
+    //                 else i--;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             if(s[i]-'A' == s[i+1]-'a')
+    //             {
+    //                 s.erase(s.begin()+i);
+    //                 s.erase(s.begin()+i);
+    //                 if(i!=0) i-=2;
+    //                 else i--;
+    //             }
+    //         }
+    //     }
+    //     return s;
+    
     string makeGood(string s) {
-        int n=s.size();
-        for(int i=0; i<n-1; i++)
+        int i=0;
+        while(s.size() > i+1)
         {
-            if(s[i]>=97)
+            // cout<<i<<" "<<s.size()<<endl;
+            if(abs(s[i+1]-s[i]) == 32)
             {
-                if(s[i]-'a' == s[i+1]-'A')
-                {
-                    s.erase(s.begin()+i);
-                    s.erase(s.begin()+i);
-                    if(i!=0) i-=2;
-                    else i--;
-                }
+                s.erase(s.begin()+i);
+                s.erase(s.begin()+i);
+                if(i!=0)
+                   i--;
+                else
+                    continue;
             }
             else
-            {
-                if(s[i]-'A' == s[i+1]-'a')
-                {
-                    s.erase(s.begin()+i);
-                    s.erase(s.begin()+i);
-                    if(i!=0) i-=2;
-                    else i--;
-                }
+            {   
+                i++;
             }
         }
         return s;
+    
     }
 };
