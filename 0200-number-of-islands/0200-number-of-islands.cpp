b0173390efea7e9430b12e779b92c624c1fa1d85
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-08 14:20:48 +0530

    Time: 36 ms (95.83%), Space: 12.1 MB (99.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,62 @@
+// class Solution {
+// public:
+//     void dfs(vector<vector<char>>& grid, vector<vector<int>> &vis, int i, int j)
+//     {
+//         int m=grid.size();
+//         int n=grid[0].size();
+//         if(i<0 || j<0 || i>=m || j>=n || vis[i][j] || grid[i][j]=='0') return;
+//         vis[i][j]=1;
+//         dfs(grid, vis, i+1, j);
+//         dfs(grid, vis, i, j+1);
+//         dfs(grid, vis, i-1, j);
+//         dfs(grid, vis, i, j-1);
+//     }
+//     int numIslands(vector<vector<char>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>>vis(m, vector<int>(n,0));
+//         int ans=0;
+//         for(int i=0; i<m; i++)
+//         {
+//             for(int j=0; j<n; j++)
+//             {
+//                 if(!vis[i][j] && grid[i][j] == '1')
+//                 {
+//                     ans++;
+//                     dfs(grid, vis, i, j);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// no need of visited array
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, vector<vector<int>> &vis, int i, int j)
+    void dfs(vector<vector<char>>& grid,  int i, int j)
     {
         int m=grid.size();
         int n=grid[0].size();
-        if(i<0 || j<0 || i>=m || j>=n || vis[i][j] || grid[i][j]=='0') return;
-        vis[i][j]=1;
-        dfs(grid, vis, i+1, j);
-        dfs(grid, vis, i, j+1);
-        dfs(grid, vis, i-1, j);
-        dfs(grid, vis, i, j-1);
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0') return;
+        grid[i][j]='0';
+        dfs(grid, i+1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>vis(m, vector<int>(n,0));
         int ans=0;
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(!vis[i][j] && grid[i][j] == '1')
+                if(grid[i][j] == '1')
                 {
                     ans++;
-                    dfs(grid, vis, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
