5d49203a83ae6d75e99f1b45a28856a628bca145
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-14 01:40:54 +0530

    Time: 48 ms (16.67%), Space: 17.9 MB (33.33%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int K = 1e9+7;
+    int good(int low, int high, int zero, int one, int ans, vector<int> &dp)
+    {
+        if(dp[ans] != -1) return dp[ans];
+        int res=0;
+        if(ans >= low && ans <= high) res+=1;
+        if(ans + zero <= high)
+        res+= good(low, high, zero, one, ans+zero, dp) % K;
+        if(ans + one <= high)
+        res+= good(low, high, zero, one, ans+one, dp) % K;
+        return dp[ans] = res % K;
+    }
+    int countGoodStrings(int low, int high, int zero, int one) {
+        string z="",  o="";
+        int ct = zero;
+        while(ct--) z+="0";
+        ct = one;
+        while(ct--) o+="1";
+        vector<int>dp(high+1, -1);
+        return good(low, high, zero, one, 0, dp);
+    }
+};
