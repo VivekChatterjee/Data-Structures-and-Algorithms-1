7df0d1ccc08b1ef71537905591aeede89d205b45
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-20 14:04:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (29.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,57 +1,17 @@
-// space optimization
 class Solution {
 public:
+    int paths(int i, int j, vector<vector<int>>&dp)
+    {
+        if(i<0 || j<0) return 0;
+        if(i == 0 && j == 0) return 1;
+        int up = 0, left = 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        up+=paths(i-1, j, dp);
+        left+= paths(i, j-1, dp);
+        return dp[i][j] = up+left;
+    }
     int uniquePaths(int m, int n) {
-        vector<int>row(n,1);
-        for(int i=1;i<m;i++)
-        {
-            vector<int>r(n);
-            for(int j=0;j<n;j++)
-            {
-                r[j]=row[j];
-                if(j>0)
-                r[j]+=r[j-1];
-            }
-            row=r;
-        }
-        return row[n-1];
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        return paths(m-1, n-1, dp);
     }
 };
-
-
-// tabulation
-// class Solution {
-// public:
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-//         for(int i=0;i<m;i++) dp[i][0]=1;
-//         for(int j=1;j<n;j++) dp[0][j]=1;
-//         for(int i=1;i<m;i++)
-//             for(int j=1;j<n;j++)
-//                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
-//         return dp[m-1][n-1];
-//     }
-// };
-
-
-
-// memoization
-// class Solution {
-// public:
-//     int paths(int i, int j, vector<vector<int>>&dp)
-//     {
-//         if(i==0 && j==0) return 1;
-//         if(i<0 || j<0) return 0;
-//         if(dp[i][j] != -1) return dp[i][j];
-//         int up=0,left=0;
-//         // up
-//         up+=paths(i-1,j,dp);
-//         // left
-//         left+=paths(i,j-1,dp);
-//         return dp[i][j]=up+left;
-//     }
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-//         return paths(m-1,n-1,dp);
-//     }
-// };
