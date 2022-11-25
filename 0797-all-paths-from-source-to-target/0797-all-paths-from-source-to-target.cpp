1381dfa4bd0f5a8812bea966b667f369286b9aba
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-25 17:25:45 +0530

    Time: 56 ms (19.85%), Space: 15.2 MB (31.96%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     void allPaths(int row, vector<vector<int>>& graph, vector<int> v, vector<vector<int>>& ans)
     {
+        v.push_back(row);
         int n = graph.size();
-        for(auto i: graph[row])
-        {
-            v.push_back(i);
-            if(i == n-1) ans.push_back(v);
-            allPaths(i, graph, v, ans);
-            v.pop_back();
-        }
+        if(row == n-1) ans.push_back(v);
+        else for(auto cur: graph[row]) allPaths(cur, graph, v, ans);
+        v.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<vector<int>>ans;
-        vector<int>v(1, 0);
+        vector<int>v;
         allPaths(0, graph, v, ans);
         return ans;
     }
