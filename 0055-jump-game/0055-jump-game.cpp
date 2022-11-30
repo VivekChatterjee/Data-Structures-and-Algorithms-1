7dd361fe4375f881faa5d472697869f4074bbf07
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-11-30 16:39:25 +0530

    Time: 766 ms (20.76%), Space: 48.8 MB (21.24%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    bool jump(int i, vector<int>&nums, vector<int>&dp)
-    {
-        int n = nums.size();
-        if(i == n-1) return 1;
-        if(i >= n || nums[i] == 0) return 0;
-        if(dp[i] != -1) return dp[i];
-        int num = nums[i];
-        bool found = false;
-        for(int j=1; j<=num; j++)
-        {
-            dp[i] = jump(i+j, nums, dp);
-            if(dp[i]) break;
-        }
-        return dp[i];
-    }
+    // bool jump(int i, vector<int>&nums, vector<int>&dp)
+    // {
+    //     int n = nums.size();
+    //     if(i == n-1) return 1;
+    //     if(i >= n || nums[i] == 0) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int num = nums[i];
+    //     bool found = false;
+    //     for(int j=1; j<=num; j++)
+    //     {
+    //         dp[i] = jump(i+j, nums, dp);
+    //         if(dp[i]) break;
+    //     }
+    //     return dp[i];
+    // }
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size() , -1);
-        return jump(0, nums, dp);
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        // return jump(0, nums, dp);
+        dp[n-1] = 1;
+        for(int i = n-2; i >= 0; i--)
+        {
+            for(int j = 1; j <= nums[i]; j++)
+            {
+                if(i+j >= n) break;
+                if(dp[i+j])
+                {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
 };
