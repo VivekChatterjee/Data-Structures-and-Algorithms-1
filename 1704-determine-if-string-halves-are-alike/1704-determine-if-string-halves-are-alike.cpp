977f15b4792c9d81cb7364738d7a62a58d36a8e4
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-01 11:00:49 +0530

    Time: 6 ms (58.35%), Space: 6.7 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
+    void vowel(int i, int n, string &s, int &cur)
+    {
+        if(i == n) return;
+        char c = s[i];
+        c = toupper(c);
+        if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') cur+=1;
+        vowel(i+1, n, s, cur);
+    }
     bool halvesAreAlike(string s) {
         int n = s.size();
-        int vowels = 0;
-        for(char c: s)
-        {
-            c = toupper(c);
-            if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') vowels++;
-        }
-        int cur = 0;
-        for(int i=0; i<n/2; i++)
-        {
-            char c = s[i];
-            c = toupper(c);
-            if(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') cur++;
-        }
-        if(cur == vowels-cur) return 1;
-        return 0;
+        int firstHalf = 0, secondHalf = 0;
+        vowel(0, n/2, s, firstHalf);
+        vowel(n/2, n, s, secondHalf);
+        if(firstHalf == secondHalf) return 1;
+        else return 0;
     }
 };
