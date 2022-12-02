92bcfff1d4dbf30e9811583f25d0cc196b19952f
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-02 17:56:16 +0530

    Time: 350 ms (18.26%), Space: 20.9 MB (9.28%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -9,11 +9,9 @@ public:
             if(!m1.count(c)) return 0;
             m2[c]++;
         }
-        vector<int>v1, v2;
-        for(auto i:m1) v1.push_back(i.second);
-        for(auto i:m2) v2.push_back(i.second);
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end());
+        multiset<int>v1, v2;
+        for(auto i:m1) v1.insert(i.second);
+        for(auto i:m2) v2.insert(i.second);
         return (v1 == v2);
     }
 };
