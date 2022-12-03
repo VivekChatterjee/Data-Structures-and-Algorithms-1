26f00e1f19c5a8b0f89d8a4bdaf6540a827416c8
Vivek Chatterjee <77501726+VivekChatterjee@users.noreply.github.com>
2022-12-04 01:25:22 +0530

    Time: 54 ms (29.40%), Space: 7.8 MB (47.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>m;
-        for(char c:t)
-            m[c]++;
-        int count = m.size();
-        int i=0,j=0;
-        int ans=INT_MAX;
-        string res="", cur="";
-        while(j<s.size())
+        int m = s.size(), n = t.size(), mini = INT_MAX, count;
+        if(m < n) return "";
+        int ans = INT_MAX;
+        string res="";
+        unordered_map<char, int>mm;
+        for(char c: t) mm[c]++;
+        count = mm.size();
+        int i=0, j=0;
+        int start = -1,end=-1;
+        while(j < m)
         {
-            if(m.count(s[j]))
+            if(mm.count(s[j]))
             {
-                m[s[j]]--;
-                if(m[s[j]] == 0)
-                    count--;
+                mm[s[j]]--;
+                if(mm[s[j]] == 0) count--;
             }
-            while(count == 0 && i<=j)
+            while(count == 0 && i <= j)
             {
                 if(j-i+1 < ans)
                 {
-                    res = s.substr(i, j-i+1);
-                    ans= min(ans, j-i+1);
-                }
-                if(m.count(s[i]))
+                    // res = s.substr(i, j-i+1);
+                    start = i;
+                    end = j;
+                    ans = j-i+1;
+                }                    
+                if(mm.count(s[i]))
                 {
-                    m[s[i]]++;
-                    if(m[s[i]] > 0)
-                        count++;
+                    mm[s[i]]++;
+                    if(mm[s[i]] > 0) count++;
                 }
-                i++;
+                i++; 
             }
             j++;
         }
+        if(start != -1 && end != -1)
+        res = s.substr(start, end-start+1);
         return res;
     }
 };
